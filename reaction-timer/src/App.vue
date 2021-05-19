00a8098395b50415436ed0b87d5375c9df1303fc
Jonathan <jbaspeling@gmail.com>
2021-05-19 09:04:38 +0200

    Added Button and initial variables

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,15 +1,27 @@
 <template>
-<h1>Boilerplate</h1>
+  <div class="playscreen">
+    <h1>Boilerplate</h1>
+    <button @click="start">Play</button>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,5 +32,21 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  height: 5em;
+  width: 10em;
+  display: inline-block;
+  padding: 0.3em 1.2em;
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 2em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: "Roboto", sans-serif;
+  font-weight: 300;
+  color: #ffffff;
+  background-color: #4eb5f1;
+  text-align: center;
+  transition: all 0.2s;
 }
 </style>
